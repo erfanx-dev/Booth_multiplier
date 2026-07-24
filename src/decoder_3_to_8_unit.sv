892d0e917e7b1f822c1a51231be992dbead85512
erfanx-dev <mr.erfan.nasiry@gmail.com>
2026-07-24 18:09:17 +0330

    merging decoder_3_to_8_unit.sv & booth_encoder.sv into booth_encoder_unit.sv

--- src/decoder_3_to_8_unit.sv
+++
@@ -1,7 +0,0 @@
-module decoder_3_to_8_unit (
-    input  logic [2:0]    data_in,
-    input  logic          enable,
-    output logic [7:0]    data_out
-);
-    assign data_out = enable ? (1 << data_in) : 8'b00000000;
-endmodule
