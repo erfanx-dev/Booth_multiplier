6fc605288686ccae0e26a76b75bf91a21f3a9593
erfanx-dev <mr.erfan.nasiry@gmail.com>
2026-07-25 14:04:33 +0330

    Add load_cnt signal in & c_out

--- src/counter_3bit.sv
+++ src/counter_3bit.sv
@@ -2,13 +2,18 @@ module counter_3bit(
     input  logic clk,
     input  logic rst,
     input  logic enable,
+    input logic load_cnt,
     output logic [2:0] count
 );
 
     always@(posedge clk) begin
         if (rst)
             count <= 3'b000;
+        else if (load_cnt)
+            count <= 3'b000;
         else if (enable)
             count <= count + 1;
     end
+
+    assign done = (count == 3'b111) ? 1'b1 : 1'b0;
 endmodule
