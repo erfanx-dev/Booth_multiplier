2a11fda9eee6739bea5b8d4702930838c3c0eb87
erfanx-dev <mr.erfan.nasiry@gmail.com>
2026-07-25 16:25:57 +0330

    Use another archtecture of shifting

--- src/arithmetic_shift_unit.sv
+++ src/arithmetic_shift_unit.sv
@@ -1,37 +1,23 @@
-module arithmetic_shift_unit # (
-    parameter WIDTH = 16
-) (
-    input  logic                   clk,
-    input  logic                   rst,
-    input  logic [1:0]             mode,
-    input  logic [$clog2(WIDTH):0] shift_amount,
-    input  logic [WIDTH-1:0]       data_in,
-    output logic [WIDTH-1:0]       data_out
-)                    q <= data_in; 
+module double_shift_register(
+    input  logic                    clk,
+    input  logic                    rst,
+    input  logic                    enable,
+    input  logic signed [WIDTH-1:0] data_in,
 
-    /*
-        mode:
-        00 : Hold
-        01 : Arithmetic Shift Right
-        10 : Arithmetic Shift Left
-        11 : Parallel Load
-    */
+    output logic signed [WIDTH-1:0] data_out
+);
 
-    always@(posedge clk) begin
-        if (rst)
+
+    always_ff @(posedge clk or posedge rst) begin
+
+        if (rst) begin
             data_out <= '0;
-        else begin
-            case(mode)
-                2'b00:
-                    data_out <= data_out;
-                2'b01:
-                    data_out <= (data_out >>> shift_amount);
-                2'b10:
-                    data_out <= (data_out <<< shift_amount);
-                2'b11:
-                    data_out <= data_in;
-                default:
-                    data_out <= data_out;
-            endcase
         end
+
+        else if (enable) begin
+            data_out <= data_in >>> 2;
+        end
+
     end
+
+endmodule
